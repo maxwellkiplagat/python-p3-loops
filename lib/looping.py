8d3cea536ba5b6dcc462b026b9bb0d70650a60d6
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-07 16:30:59 +0300

    finished python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num=10 
+    while num >0:
+        print(num)
+        num = num-1
+        print("Happy New Year!")
+        
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_number = []
+    for number in int_list:
+        squared_number.append(number*number)
+    return squared_number
+        
 
 def fizzbuzz():
     # code goes here!
-    pass
+
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
